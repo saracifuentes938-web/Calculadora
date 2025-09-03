874f12c15f21d6e54b57d4aaac2652cf0dc5bc82
Sara <saracifuentes938@gmail.com>
2025-09-03 18:07:57 -0500

    Plantilla de calculadora

---
+++ Menu.py
@@ -0,0 +1,8 @@
+print("Menu de calculadora")
+print("1. Suma 2. Resta 3. Multiplicacion 4. Division 5. Exponente 6. Raiz cuadrada " )
+
+operacion = int (input("Digite el numero de la operacion"))
+
+Num1 = float(input("Digite primer numero"))
+Num2 = float(input("Digite segundo numero"))
+
