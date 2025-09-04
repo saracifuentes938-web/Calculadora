40a99fc8bb647951cd15b53f630c73095e31e5fb
Esteban552209 <ea054196@gmail.com>
2025-09-03 19:37:10 -0500

    Suma y Raiz de la calculadora

--- Menu.py
+++ Menu.py
@@ -1,8 +1,21 @@
+def suma(N1, N2):
+    operacion = N1 + N2
+    return "La suma de los dos numeros es: " + str(operacion)
+
+def raiz (N1, N2):
+    operacion = N1 ** (1.0 / N2)
+    return "El resultado de la raiz es: " + str(operacion)
 print("Menu de calculadora")
-print("1. Suma 2. Resta 3. Multiplicacion 4. Division 5. Exponente 6. Raiz cuadrada " )
+print("1=Suma 2=Resta 3=Multiplicacion 4=Division 5=Exponente 6=Raiz " )
 
-operacion = int (input("Digite el numero de la operacion"))
+ope = int (input("Digite el numero de la operacion: "))
 
-Num1 = float(input("Digite primer numero"))
-Num2 = float(input("Digite segundo numero"))
+Num1 = float(input("Digite primer numero: "))
+Num2 = float(input("Digite segundo numero: "))
 
+if 1 == ope:
+    sumafinal= suma(Num1, Num2)
+    print(sumafinal)
+elif 6 == ope:
+    raizfinal = raiz(Num1, Num2)
+    print(raizfinal)
